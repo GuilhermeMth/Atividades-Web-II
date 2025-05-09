a1eb75190f58320a133cfbf7024bcf9b1d9ebdd8
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-05-09 19:48:43 -0300

    fix: fixed publisher model imports

--- atividade_03/app/Models/Publisher.php
+++ atividade_03/app/Models/Publisher.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Publisher extends Model
@@ -15,3 +16,5 @@ class Publisher extends Model
         return $this->hasMany(Book::class);
     }
 }
+
+
