d21523b6bf1458316ff1eb478864988e2c626e77
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-05-10 10:41:03 -0300

    editing  author factory

--- atividade_03/database/factories/AuthorFactory.php
+++ atividade_03/database/factories/AuthorFactory.php
@@ -3,21 +3,18 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Author;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Author>
  */
 class AuthorFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+     function definition()
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'birth_date' => $this->faker->date()
         ];
     }
 }
