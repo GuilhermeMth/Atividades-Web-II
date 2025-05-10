f2b6d29a540ca286f8311e4909386d83b732a9d5
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-05-10 11:02:05 -0300

    editing book factory

--- atividade_03/database/factories/BookFactory.php
+++ atividade_03/database/factories/BookFactory.php
@@ -3,21 +3,22 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Book;
+use App\Models\Category;
+use App\Models\Author;
+use App\Models\Publisher;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
  */
 class BookFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Category::class;
+
+    public function definition()
     {
         return [
-            //
+            'name' => $this->faker->unique()->word,
         ];
     }
 }
