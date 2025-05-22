b80cc165c65c0905cb1452b7b7f8d3601610510f
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-05-21 21:38:12 -0300

    including Author controller route

--- atividade_05/routes/web.php
+++ atividade_05/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,5 +13,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('categories', CategoryController::class);
+
+Route::resource('authors', AuthorController::class);
 
 
