c5135e3a27047cd9ad2c93f627ae1a265a621089
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-05-22 21:19:30 -0300

    create book controller

--- atividade_05/routes/web.php
+++ atividade_05/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\PublisherController;
+use App\Http\Controllers\BookController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,5 +20,13 @@ Route::resource('categories', CategoryController::class);
 Route::resource('authors', AuthorController::class);
 
 Route::resource('publishers', PublisherController::class);
+
+Route::get('/books/create-id-number', [BookController::class, 'createWithId'])->name('books.create.id');
+Route::post('/books/create-id-number', [BookController::class, 'storeWithId'])->name('books.store.id');
+
+Route::get('/books/create-select', [BookController::class, 'createWithSelect'])->name('books.create.select');
+Route::post('/books/create-select', [BookController::class, 'storeWithSelect'])->name('books.store.select');
+
+Route::resource('books', BookController::class)->except(['create', 'store']);
 
 
